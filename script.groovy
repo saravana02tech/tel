205f95c3a728ccf2682fdeb784b11ec1e06223f2
saravana02tech <84562030+saravana02tech@users.noreply.github.com>
2021-06-21 17:25:04 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,17 @@
+   script {
+            // Define Variable
+             def USER_INPUT = input(
+                    message: 'User input required - Some Yes or No question?',
+                    parameters: [
+                            [$class: 'ChoiceParameterDefinition',
+                             choices: ['no','yes'].join('\n'),
+                             name: 'input',
+                             description: 'Menu - select box option']
+                    ])
+            echo "The answer is: ${USER_INPUT}"
+            if( "${USER_INPUT}" == "yes"){
+                echo 'proceed'
+            } else {
+                echo 'stop'
+            }
+        }
